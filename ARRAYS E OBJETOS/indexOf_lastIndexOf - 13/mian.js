401d771b7a68e847a40847284fa54f61d377d6d6
danilo santos <danilo.o.santos@hotmail.com>
2022-02-24 21:55:08 -0300

    um pouco do metodo slice

---
+++ ARRAYS E OBJETOS/indexOf_lastIndexOf - 13/mian.js
@@ -0,0 +1,15 @@
+let familia = ['leticia','lidiane','larissa','ingrid','danilo','simone','val','hellen ','pedro'];
+let nums = [1,2,3,4,5,6,7,8,10];
+
+// let numeros = nums.slice(2,8);
+// let integrantes = familia.slice(7);
+
+// console.log(numeros);
+// console.table(integrantes);
+
+function nomes () {
+ for(let i = 0; familia.length <= i; i++){
+   console.log(nomes)
+ }
+ }
+nomes()
